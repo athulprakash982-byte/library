c577029ee56ac968cf6d15b3222e4741c456ebf9
athul <athulprakash982@gmail.com>
2025-09-16 10:21:56 +0530

    Fixed return book functionality

--- models/user.py
+++ models/user.py
@@ -10,6 +10,12 @@ class User:
             return f"{self.name} borrowed '{book.title}'"
         return "Maximum borrowing limit reached"
     
+    def return_book(self, book):  # ← ADD THIS NEW METHOD
+        if book in self.borrowed_books:
+            self.borrowed_books.remove(book)
+            return f"{self.name} returned '{book.title}'"
+        return f"{self.name} doesn't have this book"
+    
     def show_borrowed(self):
         if not self.borrowed_books:
             return f"{self.name} has no borrowed books"
